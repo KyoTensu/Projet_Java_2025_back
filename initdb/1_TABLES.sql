ac617a3e384e5db4d192eac4159caf3bffc3c51e
Mathieuepf <mathieu.roubaud@epfedu.fr>
2024-10-14 10:30:43 +0200

    Modele BDD et valeurs par default

--- initdb/1_TABLES.sql
+++ initdb/1_TABLES.sql
@@ -28,3 +28,142 @@ create table student_course
     student_id int not null,
     course_id int not null
 );
+
+
+------------------------------------------------------------
+--        Script Postgre
+------------------------------------------------------------
+
+
+
+------------------------------------------------------------
+-- Table: Lieux
+------------------------------------------------------------
+CREATE TABLE public.Lieux(
+    lieu_id         SERIAL NOT NULL ,
+    lieu_capacity   INT  NOT NULL ,
+    lieu_name       VARCHAR (50) NOT NULL  ,
+    CONSTRAINT Lieux_PK PRIMARY KEY (lieu_id)
+)WITHOUT OIDS;
+
+
+------------------------------------------------------------
+-- Table: Concerts
+------------------------------------------------------------
+CREATE TABLE public.Concerts(
+    concert_id   SERIAL NOT NULL ,
+    lieu_id      INT  NOT NULL  ,
+    CONSTRAINT Concerts_PK PRIMARY KEY (concert_id)
+
+    ,CONSTRAINT Concerts_Lieux_FK FOREIGN KEY (lieu_id) REFERENCES public.Lieux(lieu_id)
+)WITHOUT OIDS;
+
+
+------------------------------------------------------------
+-- Table: Nationalite
+------------------------------------------------------------
+CREATE TABLE public.Nationalite(
+    nationalite_id     SERIAL NOT NULL ,
+    nationalite_name   VARCHAR (50) NOT NULL ,
+    flag               VARCHAR (50) NOT NULL  ,
+    CONSTRAINT Nationalite_PK PRIMARY KEY (nationalite_id)
+)WITHOUT OIDS;
+
+
+------------------------------------------------------------
+-- Table: Style
+------------------------------------------------------------
+CREATE TABLE public.Style(
+    style_id            SERIAL NOT NULL ,
+    style_name          VARCHAR (50) NOT NULL ,
+    style_description   VARCHAR (500) NOT NULL  ,
+    CONSTRAINT Style_PK PRIMARY KEY (style_id)
+)WITHOUT OIDS;
+
+
+------------------------------------------------------------
+-- Table: Artistes
+------------------------------------------------------------
+CREATE TABLE public.Artistes(
+    Artiste_id             SERIAL NOT NULL ,
+    artiste_name           VARCHAR (50) NOT NULL ,
+    artist_is_group        BOOL  NOT NULL ,
+    artist_date_of_birth   DATE   ,
+    style_id               INT  NOT NULL ,
+    nationalite_id         INT  NOT NULL  ,
+    CONSTRAINT Artistes_PK PRIMARY KEY (Artiste_id)
+
+    ,CONSTRAINT Artistes_Style_FK FOREIGN KEY (style_id) REFERENCES public.Style(style_id)
+    ,CONSTRAINT Artistes_Nationalite0_FK FOREIGN KEY (nationalite_id) REFERENCES public.Nationalite(nationalite_id)
+)WITHOUT OIDS;
+
+
+------------------------------------------------------------
+-- Table: Utilisateurs
+------------------------------------------------------------
+CREATE TABLE public.Utilisateurs(
+    utilisateur_id          SERIAL NOT NULL ,
+    utilisateur_firstname   VARCHAR (50) NOT NULL ,
+    utilisateur_lastname    VARCHAR (100) NOT NULL ,
+    utilisateur_password    VARCHAR (100) NOT NULL ,
+    utilisateur_mail        VARCHAR (50) NOT NULL  ,
+    CONSTRAINT Utilisateurs_PK PRIMARY KEY (utilisateur_id)
+)WITHOUT OIDS;
+
+
+------------------------------------------------------------
+-- Table: Membres
+------------------------------------------------------------
+CREATE TABLE public.Membres(
+    membre_id              SERIAL NOT NULL ,
+    membre_firstname       VARCHAR (50) NOT NULL ,
+    membre_lastname        VARCHAR (100) NOT NULL ,
+    membre_date_of_birth   DATE  NOT NULL ,
+    Artiste_id             INT  NOT NULL  ,
+    CONSTRAINT Membres_PK PRIMARY KEY (membre_id)
+
+    ,CONSTRAINT Membres_Artistes_FK FOREIGN KEY (Artiste_id) REFERENCES public.Artistes(Artiste_id)
+)WITHOUT OIDS;
+
+
+------------------------------------------------------------
+-- Table: Concert_Favoris
+------------------------------------------------------------
+CREATE TABLE public.Concert_Favoris(
+    concert_id       INT  NOT NULL ,
+    utilisateur_id   INT  NOT NULL  ,
+    CONSTRAINT Concert_Favoris_PK PRIMARY KEY (concert_id,utilisateur_id)
+
+    ,CONSTRAINT Concert_Favoris_Concerts_FK FOREIGN KEY (concert_id) REFERENCES public.Concerts(concert_id)
+    ,CONSTRAINT Concert_Favoris_Utilisateurs0_FK FOREIGN KEY (utilisateur_id) REFERENCES public.Utilisateurs(utilisateur_id)
+)WITHOUT OIDS;
+
+
+------------------------------------------------------------
+-- Table: Artiste_Favoris
+------------------------------------------------------------
+CREATE TABLE public.Artiste_Favoris(
+    Artiste_id       INT  NOT NULL ,
+    utilisateur_id   INT  NOT NULL  ,
+    CONSTRAINT Artiste_Favoris_PK PRIMARY KEY (Artiste_id,utilisateur_id)
+
+    ,CONSTRAINT Artiste_Favoris_Artistes_FK FOREIGN KEY (Artiste_id) REFERENCES public.Artistes(Artiste_id)
+    ,CONSTRAINT Artiste_Favoris_Utilisateurs0_FK FOREIGN KEY (utilisateur_id) REFERENCES public.Utilisateurs(utilisateur_id)
+)WITHOUT OIDS;
+
+
+------------------------------------------------------------
+-- Table: Artistes_Concert
+------------------------------------------------------------
+CREATE TABLE public.Artistes_Concert(
+    concert_id   INT  NOT NULL ,
+    Artiste_id   INT  NOT NULL  ,
+    CONSTRAINT Artistes_Concert_PK PRIMARY KEY (concert_id,Artiste_id)
+
+    ,CONSTRAINT Artistes_Concert_Concerts_FK FOREIGN KEY (concert_id) REFERENCES public.Concerts(concert_id)
+    ,CONSTRAINT Artistes_Concert_Artistes0_FK FOREIGN KEY (Artiste_id) REFERENCES public.Artistes(Artiste_id)
+)WITHOUT OIDS;
+
+
+
+
--- initdb/2_DEFAULT_ENTRIES.sql
+++ initdb/2_DEFAULT_ENTRIES.sql
@@ -1,25 +1,59 @@
-INSERT INTO majors (id, name, description) VALUES (1, 'Ingéniérie du Numérique', 'Ouaiiis du code partout');
-INSERT INTO majors (id, name, description) VALUES (2, 'Structure & Matériaux', 'Beaucoup de béton et des poutres (snif elle a été renomée)');
-INSERT INTO majors (id, name, description) VALUES (3, 'Aéronautique & Espace', 'Vive le vent');
-INSERT INTO majors (id, name, description) VALUES (4, 'Data Engineering', 'Trop cool plein de données à ordonner');
-INSERT INTO majors (id, name, description) VALUES (5, 'Energie & Environnement', 'On est full green');
-INSERT INTO majors (id, name, description) VALUES (6, 'Engineering Management', 'Des managers de qualité');
-INSERT INTO majors (id, name, description) VALUES (7, 'Ingénierie & Santé', 'On connait tous les os et tous les muscles du corps humain');
-INSERT INTO majors (id, name, description) VALUES (8, 'Ingénierie & Architecture durable', 'Objectif 0 carbon');
-INSERT INTO majors (id, name, description) VALUES (9, 'Design Industriel Durable', 'Ca existait pas pour la P2022 ça');
+-- INSERT INTO majors (id, name, description) VALUES (1, 'Ingéniérie du Numérique', 'Ouaiiis du code partout');
+-- INSERT INTO majors (id, name, description) VALUES (2, 'Structure & Matériaux', 'Beaucoup de béton et des poutres (snif elle a été renomée)');
+-- INSERT INTO majors (id, name, description) VALUES (3, 'Aéronautique & Espace', 'Vive le vent');
+-- INSERT INTO majors (id, name, description) VALUES (4, 'Data Engineering', 'Trop cool plein de données à ordonner');
+-- INSERT INTO majors (id, name, description) VALUES (5, 'Energie & Environnement', 'On est full green');
+-- INSERT INTO majors (id, name, description) VALUES (6, 'Engineering Management', 'Des managers de qualité');
+-- INSERT INTO majors (id, name, description) VALUES (7, 'Ingénierie & Santé', 'On connait tous les os et tous les muscles du corps humain');
+-- INSERT INTO majors (id, name, description) VALUES (8, 'Ingénierie & Architecture durable', 'Objectif 0 carbon');
+-- INSERT INTO majors (id, name, description) VALUES (9, 'Design Industriel Durable', 'Ca existait pas pour la P2022 ça');
+--
+-- INSERT INTO students (id, first_name, last_name, birthdate, major_id) VALUES (1, 'Paul', 'Harrohide', '2002-06-15', 1);
+-- INSERT INTO students (id, first_name, last_name, birthdate, major_id) VALUES (2, 'Jean', 'Bonbeur', '2001-08-21', 1);
+-- INSERT INTO students (id, first_name, last_name, birthdate, major_id) VALUES (3, 'Alain', 'Térieur', '2000-01-11', 1);
+--
+-- INSERT INTO courses (id, name, hours) VALUES (1, 'Spanish', 30);
+-- INSERT INTO courses (id, name, hours) VALUES (2, 'German', 30);
+-- INSERT INTO courses (id, name, hours) VALUES (3, 'Internet of Things', 30);
+-- INSERT INTO courses (id, name, hours) VALUES (4, 'Thermodynamic', 30);
+-- INSERT INTO courses (id, name, hours) VALUES (5, 'Anatomy', 30);
+-- INSERT INTO courses (id, name, hours) VALUES (6, 'Maths', 30);
+-- INSERT INTO courses (id, name, hours) VALUES (7, 'Java', 30);
+-- INSERT INTO courses (id, name, hours) VALUES (8, 'Lean Management', 30);
+-- INSERT INTO student_course (id, student_id, course_id) VALUES (1, 1, 7);
 
-INSERT INTO students (id, first_name, last_name, birthdate, major_id) VALUES (1, 'Paul', 'Harrohide', '2002-06-15', 1);
-INSERT INTO students (id, first_name, last_name, birthdate, major_id) VALUES (2, 'Jean', 'Bonbeur', '2001-08-21', 1);
-INSERT INTO students (id, first_name, last_name, birthdate, major_id) VALUES (3, 'Alain', 'Térieur', '2000-01-11', 1);
+INSERT INTO lieux (lieu_capacity, lieu_name) VALUES (80000, 'Stade de France');
+Insert Into lieux (lieu_capacity, lieu_name) VALUES (20300, 'Accor Arena Bercy');
 
-INSERT INTO courses (id, name, hours) VALUES (1, 'Spanish', 30);
-INSERT INTO courses (id, name, hours) VALUES (2, 'German', 30);
-INSERT INTO courses (id, name, hours) VALUES (3, 'Internet of Things', 30);
-INSERT INTO courses (id, name, hours) VALUES (4, 'Thermodynamic', 30);
-INSERT INTO courses (id, name, hours) VALUES (5, 'Anatomy', 30);
-INSERT INTO courses (id, name, hours) VALUES (6, 'Maths', 30);
-INSERT INTO courses (id, name, hours) VALUES (7, 'Java', 30);
-INSERT INTO courses (id, name, hours) VALUES (8, 'Lean Management', 30);
-INSERT INTO student_course (id, student_id, course_id) VALUES (1, 1, 7);
+INSERT INTO concerts (lieu_id) VALUES (1);
+INSERT INTO concerts (lieu_id) VALUES (2);
 
+INSERT INTO nationalite (nationalite_name, flag) VALUES ('France', 'france.png');
+INSERT INTO nationalite (nationalite_name, flag) VALUES ('Canada', 'canada.png');
+INSERT INTO nationalite (nationalite_name, flag) VALUES ('USA', 'usa.png');
+INSERT INTO nationalite (nationalite_name, flag) VALUES ('Royaume-Unis', 'uk.png');
 
+INSERT INTO style (style_name, style_description) VALUES ('Pop', 'ceci est la Pop');
+INSERT INTO style (style_name, style_description) VALUES ('Rock', 'ceci est le Rock');
+INSERT INTO style (style_name, style_description) VALUES ('Variété', 'ceci est la Variété');
+
+INSERT INTO artistes (artiste_name, artist_is_group, artist_date_of_birth, style_id, nationalite_id) VALUES ('Francis Cabrel', false, '1953-11-23', 3, 1);
+INSERT INTO artistes (artiste_name, artist_is_group, artist_date_of_birth, style_id, nationalite_id) VALUES ('Queen', true, '1970-01-01', 2, 3);
+
+insert into membres (membre_firstname, membre_lastname, membre_date_of_birth, artiste_id) VALUES ('Freddie', 'Mercury', '1946-09-05', 2);
+insert into membres (membre_firstname, membre_lastname, membre_date_of_birth, artiste_id) VALUES ('Brian', 'May', '1946-09-05', 2);
+insert into membres (membre_firstname, membre_lastname, membre_date_of_birth, artiste_id) VALUES ('John', 'Deacon', '1946-09-05', 2);
+insert into membres (membre_firstname, membre_lastname, membre_date_of_birth, artiste_id) VALUES ('Roger', 'Taylor', '1946-09-05', 2);
+
+INSERT INTO utilisateurs (utilisateur_firstname, utilisateur_lastname, utilisateur_password, utilisateur_mail) VALUES ('Mathieu', 'Roubaud', '123456', 'mathieu.roubaud@epfedu.fr');
+INSERT INTO utilisateurs (utilisateur_firstname, utilisateur_lastname, utilisateur_password, utilisateur_mail) VALUES ('Loic', 'Champeau', '123456', 'loic.champeau@epfedu.fr');
+INSERT INTO utilisateurs (utilisateur_firstname, utilisateur_lastname, utilisateur_password, utilisateur_mail) VALUES ('Victor', 'Gaspard', '123456', 'victor.gaspard@epfedu.fr');
+
+Insert Into concert_favoris (concert_id, utilisateur_id) VALUES (1, 1);
+Insert Into concert_favoris (concert_id, utilisateur_id) VALUES (2, 1);
+
+INSERT INTO artiste_favoris (artiste_id, utilisateur_id) VALUES (1,1);
+INSERT INTO artiste_favoris (artiste_id, utilisateur_id) VALUES (1,2);
+
+INSERT INTO artistes_concert (concert_id, artiste_id) VALUES (1, 1);
+INSERT INTO artistes_concert (concert_id, artiste_id) VALUES (1, 2);
